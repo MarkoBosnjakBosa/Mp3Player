d57489c7f0336b15ed75d5df703bf130616d4cde
Bosa7 <marko.bosnjak.bosa.93@gmail.com>
2020-11-24 09:42:46 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Mp3Player - application for listening to music
+
+Technologies: Node.js (Express.js, Mongoose, Multer), Vue.js (VueRouter, Axios), MongoDB, JavaScript, HTML, CSS, Bootstrap
---
+++ server/app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+const models = require("./models/models.js")(mongoose);
+const multer = require("multer");
+const fs = require("fs");
+const dontenv = require("dotenv").config();
+const baseUrl = process.env.BASE_URL;
+const port = process.env.PORT;
+const databaseUrl = process.env.DATABASE_URL;
+app.use(cors({origin: "*"}));
+app.use(express.json());
+
+const music = require("./routes/music.js")(app, models, multer, fs);
+
+mongoose.connect(databaseUrl, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
+const database = mongoose.connection;
+database.on("error", function(error) {
+    console.log("Connection to the database has not been established!");
+    console.log(error);
+});
+database.on("open", function() {
+    console.log("Connection to the database has been successfully established!");
+});
+
+app.listen(port, function() {
+    console.log("Tasks app listening on " + baseUrl + port + "!");
+});
